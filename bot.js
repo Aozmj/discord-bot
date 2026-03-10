7ef8eab02985a37ca03548a0f20cf2a1ba48842a
Aozmj <a.y.m-18@hotmail.com>
2026-03-10 17:01:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,11 +11,11 @@ client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 
   setInterval(() => {
-    const channel = client.channels.cache.get(CHANNEL_ID);
+    const channel = client.channels.cache.get(1408421177268895774);
     if(channel){
       channel.send("📢 رسالة تلقائية");
     }
   }, 3600000);
 });
 
-client.login(TOKEN);
+client.login(MTQ4MDY0MDcxMDQ3Nzg3MzIwMg.Gsan0E.A-wu5m5fpr_2dlzQ3oMI_tzBf1y1vEWd3bVHBI);
