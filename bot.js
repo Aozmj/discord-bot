4e1d8818636a7ce18c0876bdab95ef1044e07fb9
Aozmj <a.y.m-18@hotmail.com>
2026-03-10 16:46:49 +0300

    Implement hourly automated message in Discord bot
    This bot sends an automated message every hour to a specified channel.

---
+++ bot.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+});
+
+const TOKEN = process.env.TOKEN;
+const CHANNEL_ID = "PUT_CHANNEL_ID";
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  setInterval(() => {
+    const channel = client.channels.cache.get(CHANNEL_ID);
+    if(channel){
+      channel.send("📢 رسالة تلقائية");
+    }
+  }, 3600000);
+});
+
+client.login(TOKEN);
